902346f301664e0388e7c92dc0115829392ed9ef
Pozharskyi <pozar1288@gmail.com>
2022-11-21 19:24:30 +0200

    HOMEWORK1

---
+++ homework1.js
@@ -0,0 +1,66 @@
+// task2
+
+// console.log("Пожарський");
+
+// task3
+
+// let someString = "runner file";
+// let someNumber = 101;
+// console.log(someString);
+// console.log(someNumber);
+// someString = someNumber;
+// console.log(someString);
+// console.log(someNumber);
+
+// task4
+
+// let player = {
+//     name: "John", 
+//     level: 85,
+//     top: true,
+//     grind: undefined,
+//     health: null
+// };
+
+// console.log(player);
+
+// task5
+
+// let isAdult = confirm("Вам виповнилось 18 років?");
+// console.log(isAdult);
+
+// task6
+
+// let firsName = "Євгеній",
+//     lastName = "Пожарський",
+//     studyGroup = 1,
+//     yearOfBirth = 1995,
+//     isMaried = false,
+//     happiness = undefined,
+//     angry = null;
+// console.log(typeof(studyGroup));
+// console.log(typeof(yearOfBirth));
+// console.log(typeof(isMaried));
+// console.log(typeof(firsName));
+// console.log(typeof(lastName));
+// console.log(typeof(happiness));
+// console.log(typeof(angry));
+
+// task7
+
+// let login = prompt("Please enter your login:"),
+//     email = prompt("Please enter your email:"),
+//     password =prompt("Please enter your password:");
+// console.log("Dear User, login is " + login + ", your email is " + email + ", your password is " + password);
+
+// task8
+
+// let hours = prompt("Ведіть кількість годин:");
+// console.log("В даній кількості годин " + (hours * 60) * 60 + " секунд");
+// alert("В даній кількості годин " + (hours * 60) * 60 + " секунд");
+// let day = prompt("Ведіть кількість днів:");
+// console.log("В даній кількості днів " + ((day * 24) * 60) * 60 + " секунд");
+// alert("В даній кількості днів " + ((day * 24) * 60) * 60 + " секунд");
+// let moon = prompt("Ведіть кількість місяців:");
+// console.log("В даній кількості місяців " + (((moon * 31) * 24) * 60) * 60 + " секунд");
+// alert("В даній кількості місяців " + (((moon * 31) * 24) * 60) * 60 + " секунд");
